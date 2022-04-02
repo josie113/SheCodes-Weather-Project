f7f867baca6494806c75ac1c67d1ccf034a3e1a7
josie113 <97979721+josie113@users.noreply.github.com>
2022-04-02 12:54:17 -0700

    changed open code link

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row gx-1 mb-3">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML =
         forecastHTML +
         `
